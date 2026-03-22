3b04c8cab889605e20b9d9de7179fdc80c39641f
krishu2814 <krishukumarsingh06@gmail.com>
2026-03-22 18:06:44 +0530

    Fixed minor bug.

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -7,7 +7,7 @@ const userController = new UserController();
 // router.post('/signup', userController.createUser.bind(userController));
 router.get('/user/email/:email', userController.getUserByEmail.bind(userController));
 router.get('/user/:id', userController.getUserById.bind(userController));
-router.put('/user/:id', userController.updateUser.bind(userController));
+router.patch('/user/:id', userController.updateUser.bind(userController));
 router.delete('/user/:id', userController.deleteUser.bind(userController));
 router.post('/signup', userController.signUp.bind(userController));
 router.post('/login', userController.signIn.bind(userController));
